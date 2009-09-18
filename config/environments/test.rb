bd4664bb624389d3008fd83d0c4ef65a833a52cf
Edoardo "Dado" Marcora <edoardo.marcora@gmail.com>
2009-09-18 11:50:47 +0100

    Added gems dependencies to test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,4 +30,6 @@ config.action_mailer.delivery_method = :test
 # thoughtbot testing
 config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => "http://gems.github.com"
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
-config.gem 'webrat'  
+config.gem 'webrat'
+config.gem 'selenium-client'
+config.gem 'selenium-rails'
